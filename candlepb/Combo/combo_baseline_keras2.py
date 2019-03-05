413296dc983fea2fa4ceb067ac00704334ceebe3
Deathn0t <romainegele@gmail.com>
2019-03-05 11:55:13 +0100

    updated

--- candlepb/Combo/combo_baseline_keras2.py
+++ candlepb/Combo/combo_baseline_keras2.py
@@ -744,10 +744,47 @@ def log_evaluation(metric_outputs, description='Comparing y_true and y_pred:'):
     for metric, value in metric_outputs.items():
         logger.info('  {}: {:.4f}'.format(metric, value))
 
+from time import time
+from deephyper.benchmark.util import numpy_dict_cache
+
+@numpy_dict_cache('~/data.npz')
+def combo_ld_numpy(args):
+
+    # CANDLE
+
+    loader = ComboDataLoader(seed=args.rng_seed,
+                             val_split=args.validation_split,
+                             cell_features=args.cell_features,
+                             drug_features=args.drug_features,
+                             response_url=args.response_url,
+                             use_landmark_genes=args.use_landmark_genes,
+                             preprocess_rnaseq=args.preprocess_rnaseq,
+                             exclude_cells=args.exclude_cells,
+                             exclude_drugs=args.exclude_drugs,
+                             use_combo_score=args.use_combo_score,
+                             cv_partition=args.cv_partition, cv=args.cv)
+
+    x_train_list, y_train, x_val_list, y_val, df_train, df_val = loader.load_data()
+
+    data = {
+        'x_train_0': x_train_list[0],
+        'x_train_1': x_train_list[1],
+        'x_train_2': x_train_list[2],
+        'y_train': y_train,
+        'x_val_0': x_val_list[0],
+        'x_val_1': x_val_list[1],
+        'x_val_2': x_val_list[2],
+        'y_val': y_val
+    }
+
+    return data
+
 def run_model(config):
 
     # config['create_structure']['func'] = util.load_attr_from(
     #     config['create_structure']['func'])
+
+    t1 = time() # T1
 
     input_shape =  [(942, ), (3820, ), (3820, )]
     output_shape = (1, )
@@ -771,70 +808,22 @@ def run_model(config):
     plot_model(model, to_file='model.png', show_shapes=True)
     model.summary()
 
-
-    # CANDLE
     params = initialize_parameters()
     args = Struct(**params)
     set_seed(args.rng_seed)
     ext = extension_from_parameters(args)
     verify_path(args.save)
-    prefix = args.save + ext
-    logfile = args.logfile if args.logfile else prefix+'.log'
-    set_up_logger(logfile, args.verbose)
-    logger.info('Params: {}'.format(params))
 
-    loader = ComboDataLoader(seed=args.rng_seed,
-                             val_split=args.validation_split,
-                             cell_features=args.cell_features,
-                             drug_features=args.drug_features,
-                             response_url=args.response_url,
-                             use_landmark_genes=args.use_landmark_genes,
-                             preprocess_rnaseq=args.preprocess_rnaseq,
-                             exclude_cells=args.exclude_cells,
-                             exclude_drugs=args.exclude_drugs,
-                             use_combo_score=args.use_combo_score,
-                             cv_partition=args.cv_partition, cv=args.cv)
+    t2 = time() # T2
+    t3 = time() # T3
+    t4 = time() # T4
+    t5 = time() # T5
+    t6 = time() # T6
 
-    train_gen = ComboDataGenerator(loader, batch_size=args.batch_size).flow()
-    val_gen = ComboDataGenerator(loader, partition='val', batch_size=args.batch_size).flow()
-
-    train_steps = int(loader.n_train / args.batch_size)
-    val_steps = int(loader.n_val / args.batch_size)
-
-    # model = build_model(loader, args, verbose=True)
-
-    # print('Creating model PNG')
-    # from keras.utils import plot_model
-    # plot_model(model, 'model_global_combo.png', show_shapes=True)
-    # print('Model PNG has been created successfuly!')
-
-    # model.summary()
-    # plot_model(model, to_file=prefix+'.model.png', show_shapes=True)
-
-    if args.cp:
-        model_json = model.to_json()
-        with open(prefix+'.model.json', 'w') as f:
-            print(model_json, file=f)
-
-    def warmup_scheduler(epoch):
-        lr = args.learning_rate or base_lr * args.batch_size/100
-        if epoch <= 5:
-            K.set_value(model.optimizer.lr, (base_lr * (5-epoch) + lr * epoch) / 5)
-        logger.debug('Epoch {}: lr={}'.format(epoch, K.get_value(model.optimizer.lr)))
-        return K.get_value(model.optimizer.lr)
-
-    df_pred_list = []
-
-    cv_ext = ''
     cv = args.cv if args.cv > 1 else 1
 
     fold = 0
     while fold < cv:
-        if args.cv > 1:
-            logger.info('Cross validation fold {}/{}:'.format(fold+1, cv))
-            cv_ext = '.cv{}'.format(fold+1)
-
-        # model = build_model(loader, args)
 
         optimizer = optimizers.deserialize({'class_name': args.optimizer, 'config': {}})
         base_lr = args.base_lr or K.get_value(optimizer.lr)
@@ -843,97 +832,36 @@ def run_model(config):
 
         model.compile(loss=args.loss, optimizer=optimizer, metrics=[mae, r2])
 
-        # calculate trainable and non-trainable params
-        params.update(candle.compute_trainable_params(model))
+        t8 = time()
+        data = combo_ld_numpy(args)
+        x_train_list = [data['x_train_0'], data['x_train_1'], data['x_train_2']]
+        y_train = data['y_train']
+        x_val_list = [data['x_val_0'], data['x_val_1'], data['x_val_2']]
+        y_val = data['y_val']
+        t7 = time() # T7
 
-        candle_monitor = candle.CandleRemoteMonitor(params=params)
-        timeout_monitor = candle.TerminateOnTimeOut(params['timeout'])
+        timing = {
+            'from_start': t7 - t1,
+            'train_load': t5 - t4,
+            'valid_load': t6 - t5,
+            'data_loader': t4 - t3,
+            'after_loading_before_fit': t7 - t6,
+            'first_part': t2 - t1,
+            'second_part': t3 - t2,
+            'both_parts': t3 - t1,
+            'load_data': t7 - t8,
+        }
 
-        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, min_lr=0.00001)
-        warmup_lr = LearningRateScheduler(warmup_scheduler)
-        checkpointer = ModelCheckpoint(prefix+cv_ext+'.weights.h5', save_best_only=True, save_weights_only=True)
-        tensorboard = TensorBoard(log_dir="tb/tb{}{}".format(ext, cv_ext))
-        history_logger = LoggingCallback(logger.debug)
-        model_recorder = ModelRecorder()
+        from pprint import pprint
+        pprint(timing)
 
-        # callbacks = [history_logger, model_recorder]
-        callbacks = [candle_monitor, timeout_monitor, history_logger, model_recorder]
-        if args.reduce_lr:
-            callbacks.append(reduce_lr)
-        if args.warmup_lr:
-            callbacks.append(warmup_lr)
-        if args.cp:
-            callbacks.append(checkpointer)
-        if args.tb:
-            callbacks.append(tensorboard)
+        history = model.fit(x_train_list, y_train,
+                            batch_size=args.batch_size,
+                            shuffle=args.shuffle,
+                            epochs=args.epochs,
+                            validation_data=(x_val_list, y_val))
 
-        if args.gen:
-            history = model.fit_generator(train_gen, train_steps,
-                                          epochs=args.epochs,
-                                          callbacks=callbacks,
-                                          validation_data=val_gen, validation_steps=val_steps)
-            fold += 1
-        else:
-            if args.cv > 1:
-                x_train_list, y_train, x_val_list, y_val, df_train, df_val = loader.load_data_cv(fold)
-            else:
-                x_train_list, y_train, x_val_list, y_val, df_train, df_val = loader.load_data()
 
-            y_shuf = np.random.permutation(y_val)
-            log_evaluation(evaluate_prediction(y_val, y_shuf),
-                           description='Between random pairs in y_val:')
-            history = model.fit(x_train_list, y_train,
-                                batch_size=args.batch_size,
-                                shuffle=args.shuffle,
-                                epochs=args.epochs,
-                                callbacks=callbacks,
-                                validation_data=(x_val_list, y_val))
-
-        if args.cp:
-            model.load_weights(prefix+cv_ext+'.weights.h5')
-
-        if not args.gen:
-            y_val_pred = model.predict(x_val_list, batch_size=args.batch_size).flatten()
-            scores = evaluate_prediction(y_val, y_val_pred)
-            if args.cv > 1 and scores[args.loss] > args.max_val_loss:
-                logger.warn('Best val_loss {} is greater than {}; retrain the model...'.format(scores[args.loss], args.max_val_loss))
-                continue
-            else:
-                fold += 1
-            log_evaluation(scores)
-            df_val.is_copy = False
-            df_val['GROWTH_PRED'] = y_val_pred
-            df_val['GROWTH_ERROR'] = y_val_pred - y_val
-            df_pred_list.append(df_val)
-
-        if args.cp:
-            # model.save(prefix+'.model.h5')
-            model_recorder.best_model.save(prefix+'.model.h5')
-
-            # test reloadded model prediction
-            # new_model = keras.models.load_model(prefix+'.model.h5')
-            # new_model.load_weights(prefix+cv_ext+'.weights.h5')
-            # new_pred = new_model.predict(x_val_list, batch_size=args.batch_size).flatten()
-            # print('y_val:', y_val[:10])
-            # print('old_pred:', y_val_pred[:10])
-            # print('new_pred:', new_pred[:10])
-
-        plot_history(prefix, history, 'loss')
-        plot_history(prefix, history, 'r2')
-
-        if K.backend() == 'tensorflow':
-            K.clear_session()
-
-    if not args.gen:
-        pred_fname = prefix + '.predicted.growth.tsv'
-        if args.use_combo_score:
-            pred_fname = prefix + '.predicted.score.tsv'
-        df_pred = pd.concat(df_pred_list)
-        df_pred.to_csv(pred_fname, sep='\t', index=False, float_format='%.4g')
-
-    logger.handlers = []
-
-    return history
 
 def load_data_combo():
     params = initialize_parameters()
