1ff3c09af0f1ce5691dc92a3c23262d7927023eb
Deathn0t <romainegele@gmail.com>
2019-03-19 21:04:19 +0100

    update for nt3

--- candlepb/NT3/nt3_baseline_keras2.py
+++ candlepb/NT3/nt3_baseline_keras2.py
@@ -6,14 +6,13 @@ import os
 import sys
 import gzip
 
-from tensorflow.keras import backend as K
+from keras import backend as K
 
-from tensorflow.keras.layers import Input, Dense, Dropout, Activation, Conv1D, MaxPooling1D, Flatten
-from tensorflow.keras.optimizers import SGD, Adam, RMSprop
-from tensorflow.keras.models import Sequential, Model, model_from_json, model_from_yaml
+from keras.layers import Input, Dense, Dropout, Activation, Conv1D, MaxPooling1D, Flatten
+from keras.optimizers import SGD, Adam, RMSprop
+from keras.models import Sequential, Model, model_from_json, model_from_yaml
 from keras.utils import np_utils
-from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger, ReduceLROnPlateau
-from tensorflow.keras.utils import plot_model
+from keras.callbacks import ModelCheckpoint, CSVLogger, ReduceLROnPlateau
 
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, MaxAbsScaler
@@ -22,6 +21,8 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler, MaxAbsScaler
 
 import candlepb.NT3.nt3 as bmk
 import candlepb.common.candle_keras as candle
+
+from deephyper.benchmark.util import numpy_dict_cache
 
 def initialize_parameters():
 
@@ -35,7 +36,7 @@ def initialize_parameters():
 
     return gParameters
 
-def load_data(train_path, test_path, gParameters):
+def load_data2(train_path, test_path, gParameters):
 
     print('Loading data...')
     df_train = (pd.read_csv(train_path,header=None).values).astype('float32')
@@ -56,9 +57,6 @@ def load_data(train_path, test_path, gParameters):
     df_x_train = df_train[:, 1:seqlen].astype(np.float32)
     df_x_test = df_test[:, 1:seqlen].astype(np.float32)
 
-#        X_train = df_x_train.as_matrix()
-#        X_test = df_x_test.as_matrix()
-
     X_train = df_x_train
     X_test = df_x_test
 
@@ -69,11 +67,14 @@ def load_data(train_path, test_path, gParameters):
     X_train = mat[:X_train.shape[0], :]
     X_test = mat[X_train.shape[0]:, :]
 
+    X_train = np.expand_dims(X_train, axis=2)
+    X_test = np.expand_dims(X_test, axis=2)
+
     return X_train, Y_train, X_test, Y_test
 
-
-def run(gParameters):
-
+@numpy_dict_cache('/projects/datascience/regele/data-tmp/nt3_data.npz')
+def load_data1():
+    gParameters = initialize_parameters()
     print ('Params:', gParameters)
 
     file_train = gParameters['train_data']
@@ -83,188 +84,28 @@ def run(gParameters):
     train_file = candle.get_file(file_train, url+file_train, cache_subdir='Pilot1')
     test_file = candle.get_file(file_test, url+file_test, cache_subdir='Pilot1')
 
-    X_train, Y_train, X_test, Y_test = load_data(train_file, test_file, gParameters)
+    X_train, Y_train, X_test, Y_test = load_data2(train_file, test_file, gParameters)
 
     print('X_train shape:', X_train.shape)
     print('X_test shape:', X_test.shape)
 
     print('Y_train shape:', Y_train.shape)
     print('Y_test shape:', Y_test.shape)
+    data = {
+        'X_train': X_train,
+        'Y_train': Y_train,
+        'X_test': X_test,
+        'Y_test': Y_test
+    }
+    return data
 
-    x_train_len = X_train.shape[1]
+@numpy_dict_cache('/dev/shm/nt3_data.py')
+def load_data_proxy():
+    return load_data1()
 
-    # this reshaping is critical for the Conv1D to work
-
-    X_train = np.expand_dims(X_train, axis=2)
-    X_test = np.expand_dims(X_test, axis=2)
-
-    print('X_train shape:', X_train.shape)
-    print('X_test shape:', X_test.shape)
-
-    model = Sequential()
-
-    layer_list = list(range(0, len(gParameters['conv']), 3))
-    for l, i in enumerate(layer_list):
-        filters = gParameters['conv'][i]
-        filter_len = gParameters['conv'][i+1]
-        stride = gParameters['conv'][i+2]
-        print(int(i/3), filters, filter_len, stride)
-        if gParameters['pool']:
-            pool_list=gParameters['pool']
-            if type(pool_list) != list:
-                pool_list=list(pool_list)
-
-        if filters <= 0 or filter_len <= 0 or stride <= 0:
-                break
-        if 'locally_connected' in gParameters:
-                model.add(LocallyConnected1D(filters, filter_len, strides=stride, padding='valid', input_shape=(x_train_len, 1)))
-        else:
-            #input layer
-            if i == 0:
-                model.add(Conv1D(filters=filters, kernel_size=filter_len, strides=stride, padding='valid', input_shape=(x_train_len, 1)))
-            else:
-                model.add(Conv1D(filters=filters, kernel_size=filter_len, strides=stride, padding='valid'))
-        model.add(Activation(gParameters['activation']))
-        if gParameters['pool']:
-                model.add(MaxPooling1D(pool_size=pool_list[int(i/3)]))
-
-    model.add(Flatten())
-
-    for layer in gParameters['dense']:
-        if layer:
-            model.add(Dense(layer))
-            model.add(Activation(gParameters['activation']))
-            if gParameters['drop']:
-                    model.add(Dropout(gParameters['drop']))
-    model.add(Dense(gParameters['classes']))
-    model.add(Activation(gParameters['out_act']))
-
-#Reference case
-#model.add(Conv1D(filters=128, kernel_size=20, strides=1, padding='valid', input_shape=(P, 1)))
-#model.add(Activation('relu'))
-#model.add(MaxPooling1D(pool_size=1))
-#model.add(Conv1D(filters=128, kernel_size=10, strides=1, padding='valid'))
-#model.add(Activation('relu'))
-#model.add(MaxPooling1D(pool_size=10))
-#model.add(Flatten())
-#model.add(Dense(200))
-#model.add(Activation('relu'))
-#model.add(Dropout(0.1))
-#model.add(Dense(20))
-#model.add(Activation('relu'))
-#model.add(Dropout(0.1))
-#model.add(Dense(CLASSES))
-#model.add(Activation('softmax'))
-
-    kerasDefaults = candle.keras_default_config()
-
-    # Define optimizer
-    optimizer = candle.build_optimizer(gParameters['optimizer'],
-                                                gParameters['learning_rate'],
-                                                kerasDefaults)
-
-    model.summary()
-    plot_model(model, to_file='nt3_model.png', show_shapes=True)
-    model.compile(loss=gParameters['loss'],
-                  optimizer=optimizer,
-                  metrics=[gParameters['metrics']])
-
-    output_dir = gParameters['save']
-
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    # calculate trainable and non-trainable params
-    gParameters.update(candle.compute_trainable_params(model))
-
-    # set up a bunch of callbacks to do work during model training..
-    model_name = gParameters['model_name']
-    path = '{}/{}.autosave.model.h5'.format(output_dir, model_name)
-    # checkpointer = ModelCheckpoint(filepath=path, verbose=1, save_weights_only=False, save_best_only=True)
-    csv_logger = CSVLogger('{}/training.log'.format(output_dir))
-    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=1, mode='auto', epsilon=0.0001, cooldown=0, min_lr=0)
-    candleRemoteMonitor = candle.CandleRemoteMonitor(params=gParameters)
-    timeoutMonitor = candle.TerminateOnTimeOut(gParameters['timeout'])
-    history = model.fit(X_train, Y_train,
-                    batch_size=gParameters['batch_size'],
-                    epochs=gParameters['epochs'],
-                    verbose=1,
-                    validation_data=(X_test, Y_test),
-                    callbacks = [csv_logger, reduce_lr, candleRemoteMonitor, timeoutMonitor])
-
-    score = model.evaluate(X_test, Y_test, verbose=0)
-
-    if False:
-        print('Test score:', score[0])
-        print('Test accuracy:', score[1])
-        # serialize model to JSON
-        model_json = model.to_json()
-        with open("{}/{}.model.json".format(output_dir, model_name), "w") as json_file:
-            json_file.write(model_json)
-
-        # serialize model to YAML
-        model_yaml = model.to_yaml()
-        with open("{}/{}.model.yaml".format(output_dir, model_name), "w") as yaml_file:
-            yaml_file.write(model_yaml)
-
-        # serialize weights to HDF5
-        model.save_weights("{}/{}.weights.h5".format(output_dir, model_name))
-        print("Saved model to disk")
-
-        # load json and create model
-        json_file = open('{}/{}.model.json'.format(output_dir, model_name), 'r')
-        loaded_model_json = json_file.read()
-        json_file.close()
-        loaded_model_json = model_from_json(loaded_model_json)
-
-
-        # load yaml and create model
-        yaml_file = open('{}/{}.model.yaml'.format(output_dir, model_name), 'r')
-        loaded_model_yaml = yaml_file.read()
-        yaml_file.close()
-        loaded_model_yaml = model_from_yaml(loaded_model_yaml)
-
-
-        # load weights into new model
-        loaded_model_json.load_weights('{}/{}.weights.h5'.format(output_dir, model_name))
-        print("Loaded json model from disk")
-
-        # evaluate json loaded model on test data
-        loaded_model_json.compile(loss=gParameters['loss'],
-            optimizer=gParameters['optimizer'],
-            metrics=[gParameters['metrics']])
-        score_json = loaded_model_json.evaluate(X_test, Y_test, verbose=0)
-
-        print('json Test score:', score_json[0])
-        print('json Test accuracy:', score_json[1])
-
-        print("json %s: %.2f%%" % (loaded_model_json.metrics_names[1], score_json[1]*100))
-
-        # load weights into new model
-        loaded_model_yaml.load_weights('{}/{}.weights.h5'.format(output_dir, model_name))
-        print("Loaded yaml model from disk")
-
-        # evaluate loaded model on test data
-        loaded_model_yaml.compile(loss=gParameters['loss'],
-            optimizer=gParameters['optimizer'],
-            metrics=[gParameters['metrics']])
-        score_yaml = loaded_model_yaml.evaluate(X_test, Y_test, verbose=0)
-
-        print('yaml Test score:', score_yaml[0])
-        print('yaml Test accuracy:', score_yaml[1])
-
-        print("yaml %s: %.2f%%" % (loaded_model_yaml.metrics_names[1], score_yaml[1]*100))
-
-    return history
-
-def main():
-
-    gParameters = initialize_parameters()
-    run(gParameters)
+def load_data():
+    data = load_data_proxy()
+    return (data['X_train'], data['Y_train']), (data['X_test'], data['Y_test'])
 
 if __name__ == '__main__':
-    main()
-    try:
-        K.clear_session()
-    except AttributeError:      # theano does not have this function
-        pass
+    load_data1()
--- candlepb/NT3/problems/problem_exp1.py
+++ candlepb/NT3/problems/problem_exp1.py
@@ -1,6 +1,6 @@
 from deephyper.benchmark import Problem
-from candlepb.NT3.nt3_baseline_keras2 import load_data_deephyper
-from candlepb.exp.candle_conv_mlp_2 import create_structure
+from candlepb.NT3.nt3_baseline_keras2 import load_data
+from candlepb.NT3.models.candle_conv_mlp_1 import create_structure
 from deephyper.search.nas.model.preprocessing import minmaxstdscaler
 
 # We create our Problem object with the Problem class, you don't have to name your Problem object 'Problem' it can be any name you want. You can also define different problems in the same module.
@@ -11,10 +11,7 @@ Problem.add_dim('regression', False) # NT3 is a classification problem between 2
 
 # You define how to load your data by giving a 'load_data' function. This function will return your data set following this interface: (train_X, train_y), (valid_X, valid_y). You can also add a 'kwargs' key with arguments for the load_data function.
 Problem.add_dim('load_data', {
-    'func': load_data_deephyper,
-    'kwargs': {
-        'prop': 1.,
-        }
+    'func': load_data,
 })
 
 # OPTIONAL : You define a preprocessing function which will be applied on your data before training generated models. This preprocessing function use sklearn preprocessors api.
@@ -25,9 +22,6 @@ Problem.add_dim('preprocessing', {
 # You define the create structure function. This function will return an object following the Structure interface. You can also have kwargs arguments such as 'num_cells' for this function.
 Problem.add_dim('create_structure', {
     'func': create_structure,
-    'kwargs': {
-        'num_cells': 4
-    }
 })
 
 # You define the hyperparameters used to train your generated models during the search.
@@ -35,7 +29,7 @@ Problem.add_dim('hyperparameters', {
     'batch_size': 20,
     'learning_rate': 0.01,
     'optimizer': 'adam',
-    'num_epochs': 3,
+    'num_epochs': 1,
     'loss_metric': 'categorical_crossentropy', # classification
     'metrics': ['acc']
 })
