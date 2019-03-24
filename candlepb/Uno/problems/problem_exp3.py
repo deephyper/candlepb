7afd4f4a594f5926fd93ba2e4b9e9db5927df10e
Deathn0t <romainegele@gmail.com>
2019-03-24 10:47:19 +0100

    update uno

--- candlepb/Uno/problems/problem_exp3.py
+++ candlepb/Uno/problems/problem_exp3.py
@@ -1,5 +1,4 @@
 from deephyper.benchmark import Problem
-from candlepb.Uno.uno_baseline_keras2 import load_data_proxy
 from candlepb.Uno.models.uno_mlp_3 import create_structure
 
 # We create our Problem object with the Problem class, you don't have to name your Problem object 'Problem' it can be any name you want. You can also define different problems in the same module.
